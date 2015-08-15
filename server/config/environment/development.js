5466337926a227cec4e7e275ad1f7866c7047d07
Alex Robinson <alex-robinson@live.com>
2015-08-15 15:51:53 +1000

    Initial commit

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/unihack2015-dev'
+  },
+
+  seedDB: true
+};
