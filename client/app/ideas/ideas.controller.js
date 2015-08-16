5bd59ea6dfc0a58cb9a2d61b6a073f673dd089c2
Alex Robinson <alex-robinson@live.com>
2015-08-16 11:44:23 +1000

    changes

--- client/app/ideas/ideas.controller.js
+++ client/app/ideas/ideas.controller.js
@@ -1,95 +1,127 @@
 'use strict';
 
 angular.module('unihack2015App')
-  .controller('IdeasCtrl', ['$scope', 'Idea', function ($scope, Idea) {
-    var newest = [];
-    var currPage = 0;
+    .controller('IdeasCtrl', ['$scope', 'Idea', function ($scope, Idea) {
+        var newest = [];
+        var currPage = 0;
 
-    $scope.ideas = Idea.query({page: currPage}, function (items) {
-      newest = items;
-      $scope.ideas = items;
-      currPage++;
-      updateGrid();
-    });
-
-    $scope.updateList = function (newIdea) {
-      $scope.ideas.unshift(newIdea);
-      updateGrid();
-    };
-
-    $scope.searchText = '';
-
-    function updateGrid() {
-      console.log('running');
-      $('#pinBoot').pinterest_grid({
-        no_columns: 4,
-        padding_x: 10,
-        padding_y: 10,
-        margin_bottom: 50,
-        single_column_breakpoint: 700
-      });
-    }
-
-    $scope.search = function () {
-      if ($scope.searchText == '') {
-        $scope.ideas = newest;
-        updateGrid();
-      } else {
-        Idea.search({title: $scope.searchText}, function (ideas) {
-          $scope.ideas = ideas;
-          updateGrid();
+        $scope.ideas = Idea.query({page: currPage}, function (items) {
+            newest = items;
+            $scope.ideas = items;
+            currPage++;
+            updateGrid();
         });
-      }
 
-    };
+        $scope.updateList = function (newIdea) {
+            $scope.ideas.unshift(newIdea);
+            updateGrid();
+        };
 
-    $scope.loadIdeas = function () {
-      Idea.loadData({pageId: currPage}, function (items) {
-        $scope.ideas = $scope.ideas.concat(items);
-        currPage++;
-        updateGrid();
-      });
-    };
+        $scope.searchText = '';
 
-    $scope.updateList = function (newIdea) {
-      $scope.ideas.unshift(newIdea);
-    };
+        function updateGrid() {
+            console.log('running');
+            $('#pinBoot').pinterest_grid({
+                no_columns: 4,
+                padding_x: 10,
+                padding_y: 10,
+                margin_bottom: 50,
+                single_column_breakpoint: 700
+            });
+        }
 
-    $scope.searchText = '';
+        $scope.search = function () {
+            if ($scope.searchText == '') {
+                $scope.ideas = newest;
+                updateGrid();
+            } else {
+                Idea.search({title: $scope.searchText}, function (ideas) {
+                    $scope.ideas = ideas;
+                    updateGrid();
+                });
+            }
 
-    function updateGrid() {
-      console.log('running');
-      $('#pinBoot').pinterest_grid({
-        no_columns: 4,
-        padding_x: 10,
-        padding_y: 10,
-        margin_bottom: 50,
-        single_column_breakpoint: 700
-      });
-    }
+        };
 
-    $scope.search = function () {
-      if ($scope.searchText == '') {
-        $scope.ideas = newest;
-        updateGrid();
-      } else {
-        Idea.search({title: $scope.searchText}, function (ideas) {
-          $scope.ideas = ideas;
-        });
-      }
-    };
+        $scope.loadIdeas = function () {
+            Idea.loadData({pageId: currPage}, function (items) {
+                $scope.ideas = $scope.ideas.concat(items);
+                currPage++;
+                updateGrid();
+            });
+        };
 
-    $scope.sort = function (method) {
-      Idea.query({sort: method}, function (ideas) {
-        $scope.ideas = ideas;
-      });
-    };
+        $scope.updateList = function (newIdea) {
+            $scope.ideas.unshift(newIdea);
+        };
 
+        $scope.searchText = '';
 
-    $scope.loadData = function() {
-      Idea.query({page: currPage}, function(ideas) {
-        $scope.ideas = $scope.ideas.concat(ideas);
-        currPage++;
-      });
-    }
-  }]);
+        function updateGrid() {
+            console.log('running');
+            $('#pinBoot').pinterest_grid({
+                no_columns: 4,
+                padding_x: 10,
+                padding_y: 10,
+                margin_bottom: 50,
+                single_column_breakpoint: 700
+            });
+        }
+
+        $scope.search = function () {
+            if ($scope.searchText == '') {
+                $scope.ideas = newest;
+                updateGrid();
+            } else {
+                Idea.search({title: $scope.searchText}, function (ideas) {
+                    $scope.ideas = ideas;
+                });
+            }
+        };
+
+        $scope.sort = function (method) {
+            Idea.query({sort: method}, function (ideas) {
+                $scope.ideas = ideas;
+            });
+        };
+
+        $scope.updateList = function (newIdea) {
+            $scope.ideas.unshift(newIdea);
+        };
+
+        $scope.searchText = '';
+
+        function updateGrid() {
+            console.log('running');
+            $('#pinBoot').pinterest_grid({
+                no_columns: 4,
+                padding_x: 10,
+                padding_y: 10,
+                margin_bottom: 50,
+                single_column_breakpoint: 700
+            });
+        }
+
+        $scope.search = function () {
+            if ($scope.searchText == '') {
+                $scope.ideas = newest;
+                updateGrid();
+            } else {
+                Idea.query({title: $scope.searchText}, function (ideas) {
+                    $scope.ideas = ideas;
+                });
+            }
+        }
+
+        $scope.sort = function (method) {
+            Idea.query({sort: method}, function (ideas) {
+                $scope.ideas = ideas;
+            });
+        }
+        $scope.loadData = function () {
+            Idea.query({page: currPage}, function (ideas) {
+                $scope.ideas = $scope.ideas.concat(ideas);
+                currPage++;
+            });
+        }
+    }]);
